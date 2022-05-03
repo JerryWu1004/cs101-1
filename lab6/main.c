d300d84ac6cb9bdae9569ef306430e80b8ca2745
JerryWu1004 <100186163+JerryWu1004@users.noreply.github.com>
2022-05-03 14:28:59 +0800

    Create main.c

---
+++ lab6/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int _sum(int x, int y) {
+    int num = x+y;
+    return num;
+}
+
+int _sub(int x, int y) {
+    int num = x-y;
+    return num;
+}
+
+int _mult(int x, int y) {
+    int num = x*y;
+    return num;
+}
+
+int _div(int x, int y) {
+    int num = x/y;
+    return num;
+}
+
+int _power(int x, int y) {
+    int num = x;
+    for(int i = 1; i < y; i++) {
+        num = num*x;    
+    }
+    return num;
+}
+
+int main() {
+    
+    int x, y, choice;
+    int (*ope[4])(int, int);
+    ope[0] = _sum;
+    ope[1] = _sub;
+    ope[2] = _mult;
+    ope[3] = _div;
+    ope[4] = _power;
+    
+    printf("Enter two integer: ");
+    scanf("%d%d",&x, &y);
+    
+    printf("Enter 0 to +, 1 to -, 2 to *, 3 to /, 4 to power\n");
+    scanf("%d",&choice);
+    
+    printf("Answer is %d",ope[choice](x, y));
+    return 0;
+    
+}
